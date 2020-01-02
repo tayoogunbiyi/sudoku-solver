637645caa190c5897f4edb516cc7ff7951614d6b
Eyitayo <eyitayoogunbiyi@gmail.com>
2020-01-02 17:23:23 +0100

    made the board look a bit better

--- app.js
+++ app.js
@@ -1,53 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Row from "./components/Row";
-
+import Board from "./components/Board";
 import "./index.css";
 
-const cellData = [
-  {
-    value: 8
-  },
-  {
-    value: 1
-  },
-  {
-    value: 2
-  },
-  {
-    value: 7
-  },
-  {
-    value: 3
-  },
-  {
-    value: 4
-  },
-  {
-    value: 5
-  },
-  {
-    value: 9
-  },
-  {
-    value: 6
-  }
-];
-
-const rowData = [];
-
-for (let i = 0; i < 9; i++) {
-  rowData.push(cellData);
-}
-
 class App extends React.Component {
-  renderRows(rowData) {
-    return rowData.map((cellData, idx) => {
-      return <Row key={idx} cellData={cellData} />;
-    });
-  }
   render() {
-    return <div>{this.renderRows(rowData)}</div>;
+    return <Board />;
   }
 }
 
---
+++ components/Board/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Row from "../Row";
+
+import "./index.css";
+
+const cellData = [
+  {
+    value: 8
+  },
+  {
+    value: 1
+  },
+  {
+    value: 2
+  },
+  {
+    value: 7
+  },
+  {
+    value: 3
+  },
+  {
+    value: 4
+  },
+  {
+    value: 5
+  },
+  {
+    value: 9
+  },
+  {
+    value: 6
+  }
+];
+
+const rowData = [];
+
+for (let i = 0; i < 9; i++) {
+  rowData.push(cellData);
+}
+
+class Board extends React.Component {
+  renderRows(rowData) {
+    return rowData.map((cellData, idx) => {
+      return <Row key={idx} cellData={cellData} />;
+    });
+  }
+  render() {
+    return <div className="board">{this.renderRows(rowData)}</div>;
+  }
+}
+
+export default Board;
