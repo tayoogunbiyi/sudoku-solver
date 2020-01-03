c2bf5b374452d8c2afc8bfaa3fc684b887ede5cf
Eyitayo <eyitayoogunbiyi@gmail.com>
2020-01-03 12:13:01 +0100

    implemented way to update the board by sending values

--- app.js
+++ app.js
@@ -3,12 +3,58 @@ import ReactDOM from "react-dom";
 import Board from "./components/Board";
 import "./index.css";
 import Button from "./components/Button";
+import { CellTypes } from "./constants";
+
+const cellData = [
+  {
+    value: 8,
+    cellType: CellTypes.DEFAULT
+  },
+  {
+    value: 1,
+    cellType: CellTypes.DEFAULT
+  },
+  {
+    value: 2,
+    cellType: CellTypes.GENERATED
+  },
+  {
+    value: 7,
+    cellType: CellTypes.DEFAULT
+  },
+  {
+    value: 3,
+    cellType: CellTypes.GENERATED
+  },
+  {
+    value: 4,
+    cellType: CellTypes.GENERATED
+  },
+  {
+    value: 2,
+    cellType: CellTypes.GENERATED
+  },
+  {
+    value: 9,
+    cellType: CellTypes.DEFAULT
+  },
+  {
+    value: 6,
+    cellType: CellTypes.GENERATED
+  }
+];
+
+const boardInitialData = [];
+
+for (let i = 0; i < 9; i++) {
+  boardInitialData.push([...cellData]);
+}
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Board />
+        <Board boardData={boardInitialData} />
       </div>
     );
   }
--- components/Board/index.js
+++ components/Board/index.js
@@ -1,64 +1,44 @@
 import React from "react";
 import Row from "../Row";
 import Button from "../Button";
+
 import { CellTypes } from "../../constants";
 import "./index.css";
 
-const cellData = [
-  {
-    value: 8,
-    cellType: CellTypes.DEFAULT
-  },
-  {
-    value: 1,
-    cellType: CellTypes.DEFAULT
-  },
-  {
-    value: 2,
-    cellType: CellTypes.GENERATED
-  },
-  {
-    value: 7,
-    cellType: CellTypes.DEFAULT
-  },
-  {
-    value: 3,
-    cellType: CellTypes.GENERATED
-  },
-  {
-    value: 4,
-    cellType: CellTypes.GENERATED
-  },
-  {
-    value: 2,
-    cellType: CellTypes.GENERATED
-  },
-  {
-    value: 9,
-    cellType: CellTypes.DEFAULT
-  },
-  {
-    value: 6,
-    cellType: CellTypes.GENERATED
-  }
-];
-
-const rowData = [];
-
-for (let i = 0; i < 9; i++) {
-  rowData.push(cellData);
-}
-
 class Board extends React.Component {
-  renderRows(rowData) {
-    return rowData.map((cellData, idx) => {
+  constructor(props) {
+    super(props);
+    const { boardData } = props;
+    this.state = {
+      board: boardData
+    };
+  }
+  renderRows() {
+    const value = Math.ceil(1 + Math.random() * 8);
+    const row = Math.floor(Math.random() * 8);
+    const col = Math.floor(Math.random() * 8);
+    setTimeout(() => this.updateBoardAt(value, row, col), 1000);
+    const { board } = this.state;
+    return board.map((cellData, idx) => {
       return <Row key={idx} cellData={cellData} />;
     });
   }
+  updateBoardAt = (value, row, col) => {
+    const { board } = this.state;
+    console.log(board[row]);
+    board[row][col] = {
+      value,
+      cellType: CellTypes.GENERATED
+    };
+    console.log(board[row]);
+    this.setState({
+      board
+    });
+  };
   render() {
     return (
       <div>
-        <div className="board">{this.renderRows(rowData)}</div>
+        <div className="board">{this.renderRows()}</div>
         <div className="btn-group">
           <Button buttonText="Reset Board" />
           <Button buttonText="Solve Board" />
