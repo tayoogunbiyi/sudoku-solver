bc793fa60f388c245848dd5b5b3395c7586f20d5
Eyitayo <eyitayoogunbiyi@gmail.com>
2020-01-09 10:05:10 +0100

    added method to create deep copy of board

--- util.js
+++ util.js
@@ -1,10 +1,19 @@
 import { CellTypes, Boards } from "./constants";
 
+const createDeepCopyOfBoard = originalBoard => {
+  const newBoard = [];
+  originalBoard.forEach(row => {
+    newBoard.push([...row]);
+  });
+  return newBoard;
+};
+
 const buildBoard = () => {
   const n = Boards.length - 1;
   const boardIdx = Math.floor(Math.random() * (n + 1));
   const board = Boards[boardIdx];
-  return board;
+
+  return createDeepCopyOfBoard(board);
 };
 
 module.exports = {
