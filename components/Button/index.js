a14dba53d3f9c53ab1c417198895a164bf19a9f5
Eyitayo <eyitayoogunbiyi@gmail.com>
2020-01-09 12:03:39 +0100

    added disabled prop to btn component

--- components/Button/index.js
+++ components/Button/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const Button = ({ buttonText, onClick }) => {
+const Button = ({ disabled, buttonText, onClick }) => {
   return (
-    <button onClick={onClick} className="btn">
+    <button disabled={disabled} onClick={onClick} className="btn">
       {buttonText}
     </button>
   );
