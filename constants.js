036f4356e71c699371e59ed71d62e8d6eb8ed585
Eyitayo <eyitayoogunbiyi@gmail.com>
2020-01-09 10:09:39 +0100

    new board data

--- constants.js
+++ constants.js
@@ -206,6 +206,208 @@ const Boards = [
       { value: 0, cellType: CellTypes.BLANK },
       { value: 0, cellType: CellTypes.BLANK }
     ]
+  ],
+  [
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 8, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" }
+    ],
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 5, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 9, cellType: "DEFAULT" },
+      { value: 3, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" }
+    ],
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 7, cellType: "DEFAULT" },
+      { value: 6, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 1, cellType: "DEFAULT" }
+    ],
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 3, cellType: "DEFAULT" },
+      { value: 2, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 5, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" }
+    ],
+    [
+      { value: 7, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 1, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 8, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" }
+    ],
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 9, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 6, cellType: "DEFAULT" },
+      { value: 7, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" }
+    ],
+    [
+      { value: 2, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 4, cellType: "DEFAULT" }
+    ],
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 7, cellType: "DEFAULT" },
+      { value: 4, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 1, cellType: "DEFAULT" },
+      { value: 3, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" }
+    ],
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 2, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 7, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" }
+    ]
+  ],
+  [
+    [
+      { value: 4, cellType: "DEFAULT" },
+      { value: 3, cellType: "DEFAULT" },
+      { value: 8, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 6, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" }
+    ],
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 8, cellType: "DEFAULT" },
+      { value: 2, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 6, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" }
+    ],
+    [
+      { value: 6, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 7, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 5, cellType: "DEFAULT" },
+      { value: 4, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" }
+    ],
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 2, cellType: "DEFAULT" },
+      { value: 1, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 7, cellType: "DEFAULT" }
+    ],
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 8, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 9, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" }
+    ],
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 4, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 7, cellType: "DEFAULT" },
+      { value: 2, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 1, cellType: "DEFAULT" },
+      { value: 5, cellType: "DEFAULT" }
+    ],
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 5, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 9, cellType: "DEFAULT" },
+      { value: 3, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 1, cellType: "DEFAULT" }
+    ],
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 8, cellType: "DEFAULT" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 4, cellType: "DEFAULT" }
+    ],
+    [
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" },
+      { value: 0, cellType: "BLANK" }
+    ]
   ]
 ];
 
